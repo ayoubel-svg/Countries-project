f3af4e01dc875d13e4d0d63f88f582c2e06643f5
Ayoub <ayoubtj2@gmail.com>
2023-01-07 15:41:36 +0100

    change head title

--- src/componnent/App.jsx
+++ src/componnent/App.jsx
@@ -3,6 +3,7 @@ import "../App.css";
 import { FaSearch } from "react-icons/fa";
 import Country from "./Country";
 import { nanoid } from "nanoid";
+import { Link } from "react-router-dom";
 export default function App(props) {
   const [myData, setMyData] = useState([]);
   const [country, setCountry] = useState("");
@@ -57,7 +58,9 @@ export default function App(props) {
       <div className="container">
         <div className="search-space">
           <div className="search-container" style={input_styles}>
-            <FaSearch className="search-logo" />
+            <Link to="/">
+              <FaSearch className="search-logo" />
+            </Link>
             <input
               onChange={handleCountryName}
               type="text"
